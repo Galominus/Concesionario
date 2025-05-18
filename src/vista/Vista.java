ba6a15a1effa3e88b96c45a157e15e180d18f3ae
Galo <galo81@hotmail.com>
2025-05-18 14:09:21 +0200

    Inicio del Proyecto

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -1,4 +1,16 @@
 package vista;
 
+import javax.swing.*;
+
 public class Vista {
+    private JPanel panel1;
+    private JButton btnHacerPedido;
+    private JButton btnVisualizarPedidos;
+    private JTextArea textArea1;
+    private JButton btnBajaPedido;
+    private JComboBox modeloComboBox;
+    private JComboBox motorComboBox;
+    private JComboBox colorComboBox;
+    private JComboBox pilotoAutoComboBox;
+    private JComboBox ruedasComboBox;
 }
